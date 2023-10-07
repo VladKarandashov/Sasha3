056296112de803c6df7902cb8270bf3ea40b3263
VladKarandashov <vlad.karandashov.tech@gmail.com>
2023-10-07 12:00:16 +0300

    сделал нормальный navbar

---
+++ src/main/java/com/example/sasha3/controller/mvc/NavbarController.java
@@ -0,0 +1,15 @@
+package com.example.sasha3.controller.mvc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class NavbarController {
+
+    @GetMapping("/navbar")
+    public String getNavBar() {
+        return "/components/navigationBar";
+    }
+}
